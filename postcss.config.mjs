1c170497584dbd28d37011a4fb25ea2517a272d9
Robocotik <alexander.starkin@gmail.com>
2025-02-24 15:32:11 +0300

    [FEAT] init tailwindcss

---
+++ postcss.config.mjs
@@ -0,0 +1,6 @@
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {config: './tailwind.config.js'},
+  },
+};
+export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      fontSize: {
+        ss: '100rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  plugins: [],
+};
