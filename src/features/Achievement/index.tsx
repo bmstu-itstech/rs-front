0741621d4e4c1f0e54e6e9eb7f3ef098baba7419
Robocotik <alexander.starkin@gmail.com>
2025-03-02 22:36:52 +0300

    [fix] achievements: fix lack of width, height attribute on <img>

--- src/features/Achievement/index.tsx
+++ src/features/Achievement/index.tsx
@@ -1,103 +1,97 @@
-'use client'
+'use client';
 
-import "./style.css";
-import {useState, MouseEventHandler, FC} from "react";
-import {useIsMobile} from "@/hooks";
-import LinkField from "@/shared/LinkField";
-import Image from "next/image";
+import './style.css';
+import {useState, MouseEventHandler, FC} from 'react';
+import {useIsMobile} from '@/hooks';
+import LinkField from '@/shared/LinkField';
+import Image from 'next/image';
 
 interface AchievementProps {
-    id: string;
-    title: string;
-    description: string;
-    photoUrl: string;
-    mediaUrl: string;
-    coverUrl: string;
-    compact?: boolean;
-    onClick?: MouseEventHandler<HTMLDivElement>;
+  id: string;
+  title: string;
+  description: string;
+  photoUrl: string;
+  mediaUrl: string;
+  coverUrl: string;
+  compact?: boolean;
+  onClick?: MouseEventHandler<HTMLDivElement>;
 }
 
-const Achievement: FC<AchievementProps> = (
-    {
-        title,
-        description,
-        coverUrl,
-        photoUrl,
-        mediaUrl,
-        compact = false,
-        onClick
-    }) => {
+const Achievement: FC<AchievementProps> = ({
+  title,
+  description,
+  coverUrl,
+  photoUrl,
+  mediaUrl,
+  compact = false,
+  onClick,
+}) => {
+  const [hovered, setHovered] = useState<boolean>(false);
+  const mobile = useIsMobile();
 
-    const [hovered, setHovered] = useState<boolean>(false);
-    const mobile = useIsMobile();
-
-    if (mobile) {
-        return (
-            <div
-                className="achievement-mobile-outer"
-            >
-                <Image src={coverUrl} alt=""/>
-                <div className="v-delimiter"/>
-                <div
-                    className="info"
-                >
-                    <div className="title">{title}</div>
-                    <div className="h-delimiter"/>
-                    <LinkField title="Подробнее" url={photoUrl}/>
-                </div>
-            </div>
-        );
-    }
-
-
-    if (compact) {
-
-        return (
-            <div
-                className="achievement compact"
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-                style={{background: `url(${coverUrl}) no-repeat center/cover, ${hovered ? 'rgba(193, 48, 0, 0.4)' : 'rgba(0, 0, 0, 0.5)'}`}}
-                onClick={onClick}
-            >
-                <div className="title">
-                    {hovered ? 'Подробнее' : title}
-                </div>
-            </div>
-        );
-
-    }
-
-
+  if (mobile) {
     return (
-        <div
-            className="achievement outer"
-            style={{backgroundImage: `url(${coverUrl})`}}
-            onClick={onClick}
-        >
-            <div className="inner">
-                <div className="info">
-                    <div className="title">{title}</div>
-                    <div className="description">{description}</div>
-                </div>
-                <div className="actions">
-                    <div
-                        className="button"
-                        onClick={() => window.location.href = photoUrl}
-                    >ФОТО
-                    </div>
-                    <div
-                        className="button"
-                        onClick={() => window.location.href = mediaUrl}
-                    >СМИ
-                    </div>
-                </div>
-            </div>
+      <div className='achievement-mobile-outer'>
+        <Image
+          src={coverUrl}
+          width={100}
+          height={100}
+          alt='coverUrl'
+        />
+        <div className='v-delimiter' />
+        <div className='info'>
+          <div className='title'>{title}</div>
+          <div className='h-delimiter' />
+          <LinkField title='Подробнее' url={photoUrl} />
         </div>
+      </div>
     );
+  }
 
+  if (compact) {
+    return (
+      <div
+        className='achievement compact'
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        style={{
+          background: `url(${coverUrl}) no-repeat center/cover, ${
+            hovered ? 'rgba(193, 48, 0, 0.4)' : 'rgba(0, 0, 0, 0.5)'
+          }`,
+        }}
+        onClick={onClick}>
+        <div className='title'>{hovered ? 'Подробнее' : title}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className='achievement outer'
+      style={{backgroundImage: `url(${coverUrl})`}}
+      onClick={onClick}>
+      <div className='inner'>
+        <div className='info'>
+          <div className='title'>{title}</div>
+          <div className='description'>{description}</div>
+        </div>
+        <div className='actions'>
+          <div
+            className='button'
+            onClick={() => (window.location.href = photoUrl)}>
+            ФОТО
+          </div>
+          <div
+            className='button'
+            onClick={() => (window.location.href = mediaUrl)}>
+            СМИ
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-Achievement.displayName = "Achievement";
+Achievement.displayName = 'Achievement';
 
 export default Achievement;
