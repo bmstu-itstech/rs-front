8b7defd95eabbe39a4e9a8f1172f06efd0ed5dfb
Robocotik <alexander.starkin@gmail.com>
2025-03-02 16:14:09 +0300

    [FIX] PartnersScreen: fix bug with key prop

--- src/screens/PartnersScreen/index.tsx
+++ src/screens/PartnersScreen/index.tsx
@@ -1,31 +1,30 @@
-import Image from "next/image";
-import style from "./PartnersScreen.module.css";
+import Image from 'next/image';
+import style from './PartnersScreen.module.css';
 
 const photos = [
-  "/partners/fc-rsk.png",
-  "/partners/techprosv.jpg",
-  "/partners/tbank.png",
-  "/partners/novikom.png",
-  "/partners/lipgart.png"
+  '/partners/fc-rsk.png',
+  '/partners/techprosv.jpg',
+  '/partners/tbank.png',
+  '/partners/novikom.png',
+  '/partners/lipgart.png',
 ];
 
 function PartnersScreen() {
-
   return (
-    <div className="partners-screen min-h-[100vh] !pt-[30px]">
-      <div className="subtitle">Партнёры</div>
+    <div className='partners-screen min-h-[100vh] !pt-[30px]'>
+      <div className='subtitle'>Партнёры</div>
       <div className={style.list}>
-        {photos.map(photo => (
+        {photos.map((photo, index) => (
           <img
+            key={index}
             className={style.photo}
             src={photo}
-            alt="logo of the partner"
+            alt='logo of the partner'
           />
         ))}
       </div>
     </div>
   );
-
-};
+}
 
 export default PartnersScreen;
