e0e0b5b5bb668e663be9a77aca719aa6f25209e9
Robocotik <alexander.starkin@gmail.com>
2025-03-04 15:28:39 +0300

    [fix] HardahonsScreen: fix meaduring + visual bugs

--- src/screens/HardathonsScreen/index.tsx
+++ src/screens/HardathonsScreen/index.tsx
@@ -1,38 +1,91 @@
-"use client";
+'use client';
 
-import { useIsMobile } from "@/hooks";
-import { PrimaryButton } from "@/shared";
-import { useRouter } from "next/navigation";
+import {useIsMobile} from '@/hooks';
+import {PrimaryButton} from '@/shared';
+import {useRouter} from 'next/navigation';
 
 function HardathonsScreen() {
+  const isMobile = useIsMobile();
+  const router = useRouter();
 
-    const isMobile = useIsMobile();
-    const router = useRouter();
-
-    return (
-        <div className="!pt-[30px] mb-[100px]">
-            <div className="subtitle">Хардатон 2024</div>
-            <div className="flex gap-[15px] justify-center items-center max-2xl:!my-[24px]">
-                {!isMobile && (
-                    <img className="max-w-[40vw] scale-[1.2] hidden 2xl:block" src="/mock/speaker.png" />
-                )}
-                <div className="flex flex-col gap-[30px] 2xl:!mt-[-60px]">
-                    <div className="flex flex-col h-[470px] max-sm:w-[90vw] max-sm:h-[50vh] bg-linear-to-b from-transparent to-[var(--primary-color)] rounded-[60px] border-[3px] border-[var(--primary-color)] !p-[30px]">
-                        <div className="text-[1.6rem] md:text-[2.8rem]">Example of long long long description</div>
-                        <div className="flex flex-col items-end justify-end !mt-auto">
-                            <div className="text-[1.4rem] md:text-[3rem] font-bold">— главный организатор хардатона,</div>
-                            <div className="text-[1.4rem] md:text-[3rem] font-bold">Валерий Бизиков</div>
-                        </div>
-                    </div>
-                    <div className="flex max-sm:flex-col justify-between gap-[30px]">
-                        <PrimaryButton onClick={() => router.push("/hardathons/1/details")}>Подробнее</PrimaryButton>
-                        <PrimaryButton>Подать заявку</PrimaryButton>
-                    </div>
-                </div>
+  return (
+    <div className='pt-12 mb-32 '>
+      <div className='subtitle'>Хардатон 2024</div>
+      <div className='flex gap-6 justify-center items-center max-2xl:!my-10'>
+        {!isMobile && (
+          <img
+            className='max-w-[40vw] scale-[1.2] hidden 2xl:block'
+            src='/mock/speaker.png'
+          />
+        )}
+        <div className='flex flex-col items-center gap-12 2xl:!-mt-24 md:w-[50dvw]'>
+          <div className='flex flex-col  md:gap-10 text-2xl md:text-5xl h-48r max-sm:w-[90vw] max-sm:h-[50vh] bg-linear-to-b from-transparent to-main-orange rounded-6xl border-2 border-main-orange p-12'>
+            <p className='text-wrap text-ellipsis h-full overflow-hidden'>
+              Examplefffff fffffff ffffffffffffffff fffffff ggg gggggggggg
+              gggggggggg gggggggggg eeeeeeeeeeee 2 21212 5423 32 235 2312 12
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+              architecto, obcaecati beatae fugiat totam adipisci excepturi harum
+              reiciendis necessitatibus ipsum fuga quos id iure dicta fugit quod
+              non? Earum consequatur fugit maxime ipsum molestias officiis,
+              voluptate quam accusantium, libero illum vitae doloribus ad? Quae
+              illo eligendi quod temporibus facere perferendis quisquam ad
+              error. Minus maiores inventore veritatis laboriosam in hic alias
+              voluptatum? Ullam, quis quos doloribus suscipit debitis iure ex
+              consequatur maiores. Qui accusantium nihil veritatis earum
+              doloribus error rem, fugit adipisci aperiam omnis et animi beatae
+              voluptates, ab cupiditate architecto rerum nemo, explicabo
+              quisquam nostrum voluptas pariatur magni facere! Laborum
+              aspernatur eum molestias iste dolorum expedita quisquam officia
+              error minus enim totam perferendis eligendi animi laboriosam
+              voluptatem nam saepe dolore vel, numquam deserunt dicta quas atque
+              praesentium maiores. Iure consectetur temporibus voluptate beatae
+              aut vero. Adipisci, debitis? Dolor laborum modi deleniti quo,
+              facilis nesciunt amet itaque sapiente ipsum doloribus labore
+              reiciendis sint nostrum perferendis aperiam beatae? Rerum quas
+              quasi ea provident, doloremque aut, fugit earum optio aspernatur
+              possimus minima neque dicta! Iste labore laudantium corporis
+              eligendi dolor omnis autem a tempore sunt minus accusantium
+              excepturi ex debitis cumque officia numquam, ratione, nemo
+              distinctio illum in perferendis necessitatibus, aliquam
+              doloremque? Lorem ipsum dolor, sit amet consectetur adipisicing
+              elit. Doloribus repellat modi tempora, aut voluptatibus eveniet
+              deleniti eius atque, expedita soluta beatae possimus cum iure
+              necessitatibus sint maxime. Cupiditate, porro accusantium.
+              ggggggggg ffffff ffffffffffffg fffffffffff of long long long Lorem
+              ipsum dolor, sit amet consectetur adipisicing elit. Deserunt nulla
+              cum, sit voluptatem eligendi omnis. Est unde corrupti eum,
+              necessitatibus voluptatibus totam, eaque magnam, vitae illum sit
+              quo minus explicabo! Illum aliquid cupiditate aliquam asperiores
+              eligendi quaerat possimus. Iusto, voluptate sequi molestiae,
+              magnam sed totam inventore optio beatae hic cum ipsum deleniti
+              quas natus numquam eos corporis blanditiis odit accusamus? A
+              possimus aut est expedita, aspernatur quasi quia molestiae,
+              voluptatum similique veniam officia vitae quibusdam, qui
+              consequatur earum iure dolorem repellendus blanditiis tenetur
+              quaerat. Repudiandae quam, sunt praesentium laboriosam provident
+              recusandae! Veritatis asperiores at eum quibusdam nisi ipsum
+              accusantium pariatur. description
+            </p>
+            <div className='flex flex-col font-bold  items-end justify-end mt-auto'>
+              <p className='text-end'>— главный организатор хардатона,</p>
+              <p>Валерий Бизиков</p>
             </div>
+          </div>
+          <div className='flex max-sm:flex-col justify-between gap-12 md:gap-24  w-full'>
+            <PrimaryButton
+              bgFilled
+              className='!w-full'
+              onClick={() => router.push('/hardathons/1/details')}>
+              Подробнее
+            </PrimaryButton>
+            <PrimaryButton className='!w-full' bgFilled>
+              Подать заявку
+            </PrimaryButton>
+          </div>
         </div>
-    );
-
-};
+      </div>
+    </div>
+  );
+}
 
 export default HardathonsScreen;
--- src/shared/PrimaryButton/index.tsx
+++ src/shared/PrimaryButton/index.tsx
@@ -16,7 +16,7 @@ const PrimaryButton: FC<Props> = ({
         bgFilled
           ? 'bg-main-orange hover:bg-white  hover:text-main-orange'
           : 'bg-transparent hover:bg-main-orange border-2 border-main-orange'
-      }  py-6 px-34 rounded-6xl w-auto md:w-fit cursor-pointer transition-all duration-300  ${className}`}
+      }  py-10 px-34 rounded-6xl w-auto md:w-fit cursor-pointer transition-all duration-300 text-5xl  ${className}`}
       {...props}
       onClick={onClick}>
       <div className={`uppercase text-inherit `}>{children}</div>
