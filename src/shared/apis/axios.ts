fde2ac034024a71789a86f1c45b1b9f3030afd2b
Robocotik <alexander.starkin@gmail.com>
2025-03-10 13:41:49 +0300

    [feat] axios:update main axios

--- src/shared/apis/axios.ts
+++ src/shared/apis/axios.ts
@@ -1,13 +1,9 @@
 import axios from 'axios';
+
 const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 1000,
-  headers: {
-    'X-Custom-Header': 'foobar',
-    'Content-Type': 'application/json', // Убедитесь, что тип содержимого соответствует вашему запросу
-    Accept: 'application/json', // Укажите, что ожидаете получить JSON
-    // 'Origin': 'https://your-domain.com', // Добавьте этот заголовок, если это необходимо
-  },
-  withCredentials: true, // Убедитесь, что это значение true, если сервер требует аутентификации
+  headers: {'X-Custom-Header': 'foobar'},
 });
+
 export {client};
