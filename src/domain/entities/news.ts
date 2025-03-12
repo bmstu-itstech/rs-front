97a46d20ea800a63611ef6cf23409f9dcbba00c0
Robocotik <alexander.starkin@gmail.com>
2025-03-12 17:44:48 +0300

    [feat] entites: add

--- src/domain/entities/news.ts
+++ src/domain/entities/news.ts
@@ -1,4 +1,5 @@
 export interface INewsBit {
+  // id: number;
   title: string;
   description: string;
   new_url: string;
--- src/domain/entities/partners.ts
+++ src/domain/entities/partners.ts
@@ -1,4 +1,5 @@
 export interface IPartnersBit {
+  // id: number;
   title: string;
   link: string;
   photo: string;
