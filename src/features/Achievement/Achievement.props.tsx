4848f8d2788dc4a35f9db16768bd4d0487633704
Robocotik <alexander.starkin@gmail.com>
2025-03-20 03:37:28 +0300

    [feat] compoennts: achievements

--- src/features/Achievement/Achievement.props.tsx
+++ src/features/Achievement/Achievement.props.tsx
@@ -1,11 +1,7 @@
-import type { HTMLAttributes } from 'react';
-
-export default interface Props extends HTMLAttributes<HTMLDivElement> {
-  id: string;
-  title: string;
-  description: string;
-  photo_album_url: string;
-  link_to_media: string;
-  photo: string | null;
+import type {HTMLAttributes} from 'react';
+import {IAchievementsBit} from '@/domain/entities/achievements';
+export default interface Props
+  extends IAchievementsBit,
+    Omit<HTMLAttributes<HTMLDivElement>, 'id' | 'title'> {
   compact?: boolean;
 }
