f4120a38ea92392af2a7acdecab7e2fd09b36835
Robocotik <alexander.starkin@gmail.com>
2025-03-31 14:32:09 +0300

    [feat] fix bug with  scroll of main page

--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -2,7 +2,7 @@
 
 import {Container, PrimaryButton} from '@/shared';
 import {NextPage} from 'next';
-import background_main from '@/assets/main_background.png'
+import background_main from '@/assets/main_background.png';
 
 const MainScreen: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const MainScreen: NextPage = () => {
       hasShadowBetween
       background={background_main.src}
       titleClassname='text-balance lg:text-9xl font-bold lg:font-normal w-full  lg:max-w-3/5 lg:text-left'
-      className=' lg:font-normal lg:text-left lg:text-9rxl pt-[20%] lg:pt-[10%] text-balance  '
+      className=' lg:font-normal lg:text-left lg:text-9rxl pt-[20%] lg:pt-[10%] text-balance lg:pb-12 '
       id='main'>
       <div className='flex flex-col h-full justify-around gap-12 w-full lg:max-w-3/5'>
         <p className='caption leading-normal text-balance '>
