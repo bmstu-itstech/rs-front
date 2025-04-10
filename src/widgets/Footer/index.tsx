007f3c57d9c243862507fe3eda91c49b1366f9d9
Robocotik <alexander.starkin@gmail.com>
2025-04-10 16:17:04 +0300

    [feat] footerL make height smaller

--- src/widgets/Footer/index.tsx
+++ src/widgets/Footer/index.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from 'next';
 const Footer: NextPage = () => {
   return (
-    <div className='h-32 mt-auto lg:h-48 bg-main-orange w-full flex justify-center items-center select-none'>
+    <div className='h-32 mt-auto lg:h-40 bg-main-orange w-full flex justify-center items-center select-none'>
       <p className='text-3xl lg:text-5xl'>Developed @ 2025 by ITS Tech</p>
     </div>
   );
