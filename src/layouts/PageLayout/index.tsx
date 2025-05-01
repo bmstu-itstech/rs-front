40f08b1a9461551ed0c634ce4d8a5f2f272924a6
artiomkovalev <artiomkratov@gmail.com>
2025-05-01 18:44:12 +0300

    [feat] re-fixed news displaying

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -42,6 +42,7 @@ export const PageLayout: FC<Props> = ({
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       <div
         className={`${className}  px-12 lg:px-60 pt-[3dvh] flex flex-col gap-16 h-full w-full`}>
 =======
@@ -50,6 +51,9 @@ export const PageLayout: FC<Props> = ({
 =======
       <div className={`${className}  px-12 2xl:px-60 min-w-[15px] pl-[15px] pr-[15px] pt-[3dvh] flex flex-col gap-16 h-full w-full`}>
 >>>>>>> 73b47f1 ([feat] fixed paddings in news)
+=======
+      <div className={`${className}  px-12 md:px-60 min-w-[15px] pl-[15px] pr-[15px] pt-[3dvh] flex flex-col gap-16 h-full w-full`}>
+>>>>>>> c63db80 ([feat] re-fixed news displaying)
         {title && (
           <p
             className={`text-6xl lg:text-9xl font-bold lg:font-normal leading-normal text-center select-none uppercase ${
