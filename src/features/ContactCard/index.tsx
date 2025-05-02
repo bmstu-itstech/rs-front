fe1b9aa8a368c83e9cd9afa635b0ecbcedea271b
Robocotik <alexander.starkin@gmail.com>
2025-05-02 14:28:42 +0300

    [DENY] return contacts screen to robocotik version

--- src/features/ContactCard/index.tsx
+++ src/features/ContactCard/index.tsx
@@ -5,7 +5,8 @@ import {data} from './ContactCard.usecase';
 
 const ContactCard: FC = () => {
   return (
-    <div className='min-w-[450px] min-h-[464px] max-w-[700px] max-h-[700px] border-3 border-main-orange rounded-3rxl bg-black bg-opacity-80 w-full h-full flex flex-col'>
+    <div className='border-3 border-main-orange rounded-3rxl bg-black bg-opacity-80 w-full h-full flex flex-col'>
+      {/* <div className='min-w-[450px] min-h-[464px] max-w-[700px] max-h-[700px] border-3 border-main-orange rounded-3rxl bg-black bg-opacity-80 w-full h-full flex flex-col'> */}
       <div className='flex flex-col p-8 lg:p-12 h-full  justify-evenly lg:justify-between'>
         {data.map((item, index) => {
           return (
