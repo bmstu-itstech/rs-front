daf8557f259ad2d1a1a4d088ee474da2e2826623
Robocotik <alexander.starkin@gmail.com>
2025-05-02 19:37:28 +0300

    [feat] achievements: make it sizable to all devices

--- src/features/Achievement/index.tsx
+++ src/features/Achievement/index.tsx
@@ -10,96 +10,92 @@ import no_photo from '@/assets/no_photo.jpg';
 import {useRouter} from 'next/navigation';
 
 const Achievement: FC<Props> = ({
-                                    id,
-                                    title,
-                                    description,
-                                    image,
-                                    album_url,
-                                    media_url,
-                                    compact = false,
-                                    onClick,
-                                    className,
-                                    ...props
-                                }) => {
-    const [hovered, setHovered] = useState<boolean>(false);
-    const mobile = useIsMobile();
-    const router = useRouter();
-    const curPhoto = image ?? no_photo.src;
-    if (mobile) {
-        return (
-            <div
-                className={`max-w-200 border-2 border-main-orange h-44 max-h-44 rounded-4xl flex overflow-hidden w-full ${className}`}
-                {...props}>
-                <Image
-                    width={1416}
-                    height={945}
-                    src={curPhoto}
-                    alt='achievement'
-                    className='w-1/3 h-full min-h-full object-cover object-center'
-                />
-                <Divider isVertical/>
-                <div className='flex flex-col items-center justify-evenly flex-1 gap-1 py-2 w-full'>
-                    <p className='text-2xl text-center uppercase text-ellipsis line-clamp-1 max-w-5/6 w-full'>
-                        {title}
-                    </p>
-                    <Divider/>
-                    <LinkField title='Подробнее' url={album_url}/>
-                </div>
-            </div>
-        );
-    }
-    if (compact) {
-        return (
-            <div
-                className={`w-full h-full border-5 overflow-hidden  border-main-orange hover:border-white  transition-all duration-300 flex justify-center items-center rounded-6xl cursor-pointer bg-cover bg-center bg-blend-multiply ${className}`}
-                {...props}
-                style={{
-                    backgroundImage: `url(${curPhoto})`,
-                }}
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-                onClick={onClick}>
-                <div
-                    className='text-center flex items-center transition-all justify-center  text-5xl uppercase bg-gradient-to-b from-[rgba(0,0,0,0.5)] hover:from-main-orange-50 hover:to-main-orange-50 to-transparent from-52%  h-full w-full '>
-                    {hovered ? 'Подробнее' : title}
-                </div>
-            </div>
-        );
-    }
+  id,
+  title,
+  description,
+  image,
+  album_url,
+  media_url,
+  compact = false,
+  onClick,
+  className,
+  ...props
+}) => {
+  const [hovered, setHovered] = useState<boolean>(false);
+  const mobile = useIsMobile();
+  const router = useRouter();
+  const curPhoto = image ?? no_photo.src;
+  if (mobile) {
     return (
-        <div
-            className={` w-full h-[42rem]  border-5 border-main-orange rounded-6xl overflow-hidden bg-no-repeat bg-right bg-contain   ${className}`}
-            style={{
-                backgroundImage: `url(${curPhoto})`,
-                backgroundSize: '55%', // Устанавливаем ширину фона на 50%
-            }}
-            {...props}
-            onClick={onClick}>
-            <div
-                className='px-32 flex flex-col justify-around  bg-gradient-to-r from-black to-transparent from-52% h-full'>
-                <div className='flex flex-col gap-8 max-w-1/2'>
-                    <div className='text-7xl max-w-full truncate'>{title}</div>
-                    <div
-                        className='text-4xl max-h-80 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
-                        {description}
-                    </div>
-                </div>
-                <div className='flex gap-14'>
-                    <button
-                        className='w-72 py-8 rounded-6xl bg-main-orange text-3xl  hover:bg-white hover:text-main-orange flex justify-center items-center cursor-pointer  duration-300 '
-                        onClick={() => router.push(album_url)}>
-                        ФОТО
-                    </button>
-                    <button
-                        className='w-72 py-8 rounded-6xl bg-main-orange hover:bg-white hover:text-main-orange text-white text-3xl flex justify-center items-center cursor-pointer  duration-300'
-                        onClick={() => router.push(media_url)}>
-                        СМИ
-                    </button>
-                </div>
-            </div>
+      <div
+        className={`border-2 border-main-orange h-60 max-h-60 rounded-4xl flex overflow-hidden w-full ${className}`}
+        {...props}>
+        <Image
+          width={1416}
+          height={945}
+          src={curPhoto}
+          alt='achievement'
+          className='w-1/3 h-full min-h-full object-cover object-center'
+        />
+        <Divider isVertical />
+        <div className='flex flex-col items-center justify-evenly flex-1 gap-1 py-2 w-full'>
+          <p className='text-5xl text-center uppercase text-ellipsis line-clamp-1 max-w-5/6 w-full'>
+            {title}
+          </p>
+          <Divider />
+          <LinkField title='Подробнее' url={album_url} />
         </div>
-
+      </div>
     );
+  }
+  if (compact) {
+    return (
+      <div
+        className={`w-full h-full border-5 overflow-hidden border-main-orange hover:border-white  transition-all duration-300 flex justify-center items-center md:rounded-[4rem] lg:rounded-[5rem] 2xl:rounded-6xl cursor-pointer bg-cover bg-center bg-blend-multiply ${className}`}
+        {...props}
+        style={{
+          backgroundImage: `url(${curPhoto})`,
+        }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={onClick}>
+        <p className='text-center flex items-center transition-all justify-center text-4xl  lg:text-5xl uppercase bg-gradient-to-b from-[rgba(0,0,0,0.5)] hover:from-main-orange-50 hover:to-main-orange-50 to-transparent from-52%  h-full w-full '>
+          {hovered ? 'Подробнее' : title}
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div
+      className={` w-full h-full border-5 border-main-orange md:rounded-[4rem] lg:rounded-[5rem] 2xl:rounded-6xl overflow-hidden bg-no-repeat bg-right bg-contain ${className}`}
+      style={{
+        backgroundImage: `url(${curPhoto})`,
+        backgroundSize: '55%', // Устанавливаем ширину фона на 50%
+      }}
+      {...props}
+      onClick={onClick}>
+      <div className='px-32 flex flex-col justify-around  bg-gradient-to-r from-black to-transparent from-52% h-full'>
+        <div className='flex flex-col gap-8 max-w-1/2'>
+          <div className='lg:text-6xl md:text-5xl 2xl:text-7xl max-w-full truncate'>{title}</div>
+          <p className='lg:text-3xl md:text-2xl 2xl:text-4xl max-h-40  2xl:max-h-80 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]'>
+            {description}
+          </p>
+        </div>
+        <div className='flex gap-6 2xl:gap-14'>
+          <button
+            className=' w-40 lg:w-60 2xl:w-72 py-5 lg:py-6 xl:py-8 lg:rounded-[3rem] 2xl:rounded-6xl bg-main-orange text-3xl  hover:bg-white hover:text-main-orange flex justify-center items-center cursor-pointer  duration-300 '
+            onClick={() => router.push(album_url)}>
+            ФОТО
+          </button>
+          <button
+            className='w-40 lg:w-60 2xl:w-72 py-5 lg:py-6 xl:py-8 lg:rounded-[3rem] 2xl:rounded-6xl bg-main-orange hover:bg-white hover:text-main-orange text-white text-3xl flex justify-center items-center cursor-pointer  duration-300'
+            onClick={() => router.push(media_url)}>
+            СМИ
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 Achievement.displayName = 'Achievement';
 export default Achievement;
--- src/screens/AchievementsScreen/index.tsx
+++ src/screens/AchievementsScreen/index.tsx
@@ -7,136 +7,114 @@ import {Achievement} from '@/features';
 import {FC} from 'react';
 import {useGetAchievements} from '@/hooks/Achievements/useGetAchievements';
 import {
-    MainAchievemtSkeleton,
-    SubAchievemtSkeletonPC,
-    CompactAchievemtSkeleton,
+  MainAchievemtSkeleton,
+  SubAchievemtSkeletonPC,
+  CompactAchievemtSkeleton,
 } from '@/features/Achievement/Achievement.skeleton';
 
 const AchievementsScreen: FC = () => {
-    const mobile = useIsMobile();
-    const [active, setActive] = useState(1);
-    const {data, isLoading} = useGetAchievements();
-
+  const mobile = useIsMobile();
+  const [active, setActive] = useState(0);
+  const {data, isLoading} = useGetAchievements();
 
   const handleActive = useCallback(
     (newId: number) => {
-      if (!data?.results) return;
-      const originalIndex = data.results.findIndex(
-        (vm, index) => index === newId,
-      );
-      const activeIndex = data.results.findIndex(
-        (v, index) => index === active,
-      );
-
-      if (originalIndex === -1 || activeIndex === -1) return;
-
-      const updatedData = [...data.results];
-      [updatedData[originalIndex], updatedData[activeIndex]] = [
-        updatedData[activeIndex],
-        updatedData[originalIndex],
-      ];
       setActive(newId);
     },
-    [data, active, mobile],
+    [mobile, active],
   );
 
-    const MainItemToShowPC = useCallback(() => {
-        if (isLoading || !data) {
-            return <MainAchievemtSkeleton className='mb-12'/>;
-        } else {
-            const ActiveItem = data.results.find(item => item.id == active);
-            return ActiveItem ? (
-                <Achievement className='mb-12' key={active} {...ActiveItem} />
-            ) : (
-                <Achievement className='mb-12' key={active} {...data.results[0]} />
-            );
-        }
-    }, [isLoading, active, data]);
+  const MainItemToShowPC = useCallback(() => {
+    if (isLoading || !data) {
+      return <MainAchievemtSkeleton className='mb-12' />;
+    } else {
+      const ActiveItem = data.results.find(item => item.id == active);
+      return ActiveItem ? (
+        <Achievement className='mb-12 flex-1/3' key={active} {...ActiveItem} />
+      ) : (
+        <Achievement className='mb-12 flex-1/3' key={active} {...data.results[0]} />
+      );
+    }
+  }, [isLoading, active, data]);
 
+  const SubItemsToShowPC = useCallback(() => {
+    return isLoading || !data ? (
+      <>
+        <SubAchievemtSkeletonPC />
+        <SubAchievemtSkeletonPC />
+        <SubAchievemtSkeletonPC />
+      </>
+    ) : (
+      <>
+        {data?.results
+          .filter(v1 => v1.id !== active)
+          .map(v2 => (
+            <Achievement
+              compact
+              className={`odd:flex-row-reverse `}
+              {...v2}
+              key={v2.id}
+              onClick={() => {
+                handleActive(v2.id);
+              }}
+            />
+          ))}
+      </>
+    );
+  }, [isLoading, active, data, handleActive]);
 
-    const SubItemsToShowPC = useCallback(() => {
-        return isLoading || !data ? (
-            <>
-                <SubAchievemtSkeletonPC/>
-                <SubAchievemtSkeletonPC/>
-                <SubAchievemtSkeletonPC/>
-            </>
-        ) : (
-            <>
-                {data?.results
-                    .filter(v1 => v1.id !== active)
-                    .map(v2 => (
-                        <Achievement
-                            compact
-                            className={`odd:flex-row-reverse `}
-                            {...v2}
-                            key={v2.id}
-                            onClick={() => {
-                                handleActive(v2.id);
-                                console.log('Я КЛИКНУЛ НА', v2.id);
-                            }}
-                        />
-                    ))}
-            </>
-        );
-    }, [isLoading, active, data, handleActive]);
+  const CompactItemsToShow = useCallback(() => {
+    return isLoading || !data ? (
+      <>
+        <CompactAchievemtSkeleton />
+        <CompactAchievemtSkeleton />
+        <CompactAchievemtSkeleton />
+      </>
+    ) : (
+      <>
+        {data?.results.map((v, index) => (
+          <Achievement
+            compact
+            className={`odd:flex-row-reverse snap-start snap-always `}
+            {...v}
+            key={index}
+            onClick={() => handleActive(index)}
+          />
+        ))}
+      </>
+    );
+  }, [isLoading, data, handleActive]);
 
-
-    const CompactItemsToShow = useCallback(() => {
-        return isLoading || !data ? (
-            <>
-                <CompactAchievemtSkeleton/>
-                <CompactAchievemtSkeleton/>
-                <CompactAchievemtSkeleton/>
-            </>
-        ) : (
-            <>
-                {data?.results.map((v, index) => (
-                    <Achievement
-                        compact
-                        className={`odd:flex-row-reverse snap-start snap-always `}
-                        {...v}
-                        key={index}
-                        onClick={() => handleActive(index)}
-                    />
-                ))}
-            </>
-        );
-    }, [isLoading, data, handleActive]);
-
-    const PCAchivements = () => {
-        return (
-            <div className={"flex justify-center flex-col mt-15.5"}>
-
-                <div className='flex flex-col h-full mx-auto w-full lg:w-[min(140rem,100%)]'>
-                    <MainItemToShowPC/>
-                    <div
-                        className='
-           flex-row auto-rows-[0px] overflow-hidden h-22r  lg:grid
+  const PCAchivements = () => {
+    return (
+      <div className='flex flex-col  mx-auto w-full justify-center md:px-20 lg:px-60  aspect-[2/1] max-h-260'>
+        <MainItemToShowPC />
+        <div
+          className='
+           flex-row auto-rows-[0px] overflow-hidden h-full flex-1 min-h-1/3  lg:grid
           grid-cols-3 grid-rows-1 tems-start justify-center gap-x-12
          lg:gap-y-0'>
-                        <SubItemsToShowPC/>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
-    const MobileAchievements = () => {
-        return (
-            <div
-                className='flex-col max-h-2/3 snap-start snap-mandatory overflow-y-auto
-            flex items-center justify-start gap-y-12 scroll-smooth snap-y'>
-                {<CompactItemsToShow/>}
-            </div>
-        );
-    };
-
-    return (
-        <Container hasShadowBetween title='Достижения' id='achievements'>
-            {mobile ? <MobileAchievements/> : <PCAchivements/>}
-        </Container>
+          <SubItemsToShowPC />
+        </div>
+      </div>
     );
+  };
+
+  const MobileAchievements = () => {
+    return (
+      <div
+        className='flex-col max-h-2/3 snap-start snap-mandatory overflow-y-auto
+            flex items-center justify-start gap-y-12 scroll-smooth snap-y'>
+        {<CompactItemsToShow />}
+      </div>
+    );
+  };
+
+  return (
+    <Container hasShadowBetween title='Достижения' id='achievements'>
+      {mobile ? <MobileAchievements /> : <PCAchivements />}
+    </Container>
+  );
 };
 
 AchievementsScreen.displayName = 'AchievementsScreen';
