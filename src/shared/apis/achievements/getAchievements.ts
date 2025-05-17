290863d60e37e75c54116e31d273e649af90a9d8
Robocotik <alexander.starkin@gmail.com>
2025-05-18 00:15:01 +0300

    [feat] achievements: change limit for loading to 4

--- src/shared/apis/achievements/getAchievements.ts
+++ src/shared/apis/achievements/getAchievements.ts
@@ -3,7 +3,7 @@ import {IAchievements} from '@/domain/entities/achievements';
 const achievementsList: () => Promise<IAchievements> = async () => {
   try {
     const response = await client.get(
-      `${process.env.NEXT_PUBLIC_API_URL}api/v0/achievements/?format=json&limit=10`,
+      `${process.env.NEXT_PUBLIC_API_URL}api/v0/achievements/?format=json&limit=4`,
     );
     return response.data;
   } catch (error) {
