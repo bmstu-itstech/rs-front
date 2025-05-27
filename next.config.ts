9b008c01073d2162c170b8b6ad3aaa1fbfd04ab3
zhikh <zhikh.k@gmail.com>
2025-05-27 17:55:52 +0300

    [fix] off Next images optiomization

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,26 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      },
+{
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '',
+        pathname: '/**',
+      },
+{
+        protocol: 'http',
         hostname: '91.135.156.15',
         port: '',
         pathname: '/**',
       },
     ],
+    unoptimized: true,
   },
 };
 
 export default nextConfig;
+
