eff27db076bcdb4f175230f48837afe2456387ef
Robocotik <alexander.starkin@gmail.com>
2025-06-09 21:50:41 +0300

    [feat] offcanvas: add close animation

--- src/layouts/OffcanvasLayout/index.tsx
+++ src/layouts/OffcanvasLayout/index.tsx
@@ -40,25 +40,24 @@ const OffcanvasData: FC<Props> = ({
       <button onClick={openMenu} className={`${isOpen && hideIconOnOpen && 'hidden'} ${className}`}>
         {icon}
       </button>
-      {isOpen &&
-        createPortal(
-          <>
-            {hasOverlayShadowing && (
-              <div className='absolute top-0 left-0 h-dvh w-full inset-0 bg-black/50  z-10 transition-opacity duration-300' />
-            )}
-            <div
-              ref={menuRef}
-              className={` absolute top-0 right-0 ${
-                isOpen ? 'animate-appear' : ' animate-disappear invisible'
-              } transition-all  w-dvw lg:min-w-[50rem]  lg:w-auto justify-end
+      {createPortal(
+        <>
+          {isOpen && hasOverlayShadowing && (
+            <div className='absolute top-0 left-0 h-dvh w-full inset-0 bg-black/50  z-20 transition-opacity duration-300' />
+          )}
+          <div
+            ref={menuRef}
+            className={` absolute top-0 right-0 ${
+              isOpen ? 'animate-appear' : ' animate-disappear invisible'
+            } transition-all  w-dvw lg:min-w-[50rem]  lg:w-auto justify-end
          max-h-dvh h-dvh flex items-center lg:items-stretch
-            bg-main-orange lg:bg-transparent z-10
+            bg-main-orange lg:bg-transparent z-30
               `}>
-              {children}
-            </div>
-          </>,
-          document.body,
-        )}
+            {children}
+          </div>
+        </>,
+        document.body,
+      )}
     </>
   );
 };
