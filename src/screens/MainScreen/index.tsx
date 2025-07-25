8fd86201fee6b776ceb73f853b246c1fa719441e
Robocotik <alexander.starkin@gmail.com>
2025-07-25 20:29:10 +0300

    [feat] adapt mainScrenn to mobile

--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -3,7 +3,7 @@ import {Container, PrimaryButton} from '@/shared';
 import background_main from '@/assets/main_background.png';
 import TelegramIcon from '@/shared/TelegramIcon';
 import VKIcon from '@/shared/VKIcon';
-import React, {useCallback} from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 const MainScreen = () => {
@@ -15,41 +15,49 @@ const MainScreen = () => {
       // titleClassname='text-balance !text-8xl lg:!text-9xl font-bold lg:font-normal w-full md:!text-8xl md:text-center  md:max-w-4/5 md:mx-auto lg:mx-0 lg:text-start lg:text-left'
       className=' lg:font-normal lg:text-left md:pt-12 lg:pt-[10%] text-balance lg:pb-12 '
       id='main'>
-      <div className='flex flex-col  h-full justify-center lg:justify-between items-center w-full '>
-        <h1 className='w-fit  flex flex-col text-center'>
-          <span
-            style={{animationDelay: '0s'}}
-            className='animate-text-coloring text-white lg:text-9rxl text-5rxl md:text-8xl leading-[1.5] '>
-            Центр
-          </span>
-          <div className='flex gap-8'>
+      <div className='flex flex-col h-full justify-center lg:justify-between items-center w-full '>
+        <div className='-mt-60 md:mt-60 lg:mt-0'>
+          <h1 className='w-fit flex flex-col text-center mx-auto'>
             <span
-              style={{animationDelay: '0.1s'}}
-              className='animate-text-coloring text-white lg:text-9rxl text-5rxl md:text-8xl leading-[1.5]'>
-              твоего
+              style={{animationDelay: '0s'}}
+              className='animate-text-coloring text-white lg:text-8rxl text-5rxl md:text-8xl leading-[1.5] '>
+              Центр
             </span>
+            <div className='flex gap-8'>
+              <span
+                style={{animationDelay: '0.1s'}}
+                className='animate-text-coloring text-white lg:text-8rxl text-5rxl md:text-8xl leading-[1.5]'>
+                твоего
+              </span>
+              <span
+                style={{animationDelay: '0.2s'}}
+                className='animate-text-coloring text-white lg:text-8rxl text-5rxl md:text-8xl leading-[1.5]'>
+                старта
+              </span>
+              <span
+                style={{animationDelay: '0.3s'}}
+                className='animate-text-coloring text-white lg:text-8rxl text-5rxl md:text-8xl leading-[1.5]'>
+                в
+              </span>
+              <span
+                style={{animationDelay: '0.4s'}}
+                className='animate-text-coloring text-white lg:text-8rxl text-5rxl md:text-8xl leading-[1.5]'>
+                мире
+              </span>
+            </div>
             <span
-              style={{animationDelay: '0.2s'}}
-              className='animate-text-coloring text-white lg:text-9rxl text-5rxl md:text-8xl leading-[1.5]'>
-              старта
+              style={{animationDelay: '0.5s'}}
+              className=' text-logo-orange animate-text-coloring-highlighted  lg:text-8rxl text-5rxl md:text-8xl leading-[1.5]'>
+              робототехники
             </span>
-            <span
-              style={{animationDelay: '0.3s'}}
-              className='animate-text-coloring text-white lg:text-9rxl text-5rxl md:text-8xl leading-[1.5]'>
-              в
-            </span>
-            <span
-              style={{animationDelay: '0.4s'}}
-              className='animate-text-coloring text-white lg:text-9rxl text-5rxl md:text-8xl leading-[1.5]'>
-              мире
-            </span>
-          </div>
-          <span
-            style={{animationDelay: '0.5s'}}
-            className=' text-logo-orange animate-text-coloring-highlighted  lg:text-9rxl text-5rxl md:text-8xl leading-[1.5]'>
-            робототехники
-          </span>
-        </h1>
+          </h1>
+          <p
+            className='animate-appearUp mt-48 text-center mx-auto w-fit lg:text-4xl text-4xl md:text-4xl'
+            style={{animationDelay: '1s'}}>
+            Центр молодежной робототехники МГТУ им. Н.Э. Баумана
+          </p>
+        </div>
+
         <div className='hidden lg:flex w-full justify-end items-center'>
           <Link href={'https://t.me/robotics_bmstu'}>
             <TelegramIcon className='me-12 cursor-pointer' />
