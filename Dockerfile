b821545566990dbcd53506300b24e6fb90aa3010
SHENID <dineshgembler@gmail.com>
2025-11-28 11:36:38 +0300

    [bug] edit Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -10,12 +10,14 @@ FROM node:22.16-alpine AS builder
 WORKDIR /app
 COPY . .
 COPY --from=dependencies /app/node_modules ./node_modules
+COPY .env* ./
 RUN npx next build
 
 FROM node:22.16-alpine AS runner
 WORKDIR /app
 ENV NODE_ENV=production
 
+COPY --from=builder /app/.env* ./
 COPY --from=builder /app/public ./public
 COPY --from=builder /app/package.json ./package.json
 COPY --from=builder /app/.next ./.next
--- next.config.ts
+++ next.config.ts
@@ -1,50 +1,55 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  images: {
-    remotePatterns: [
-      ...(isDev
-        ? [
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_CAN_SEND_MESSAGES: process.env.NEXT_PUBLIC_CAN_SEND_MESSAGES,
+        CORS_ORIGIN_ALLOW_ALL: process.env.CORS_ORIGIN_ALLOW_ALL,
+    },
+    images: {
+        remotePatterns: [
+            ...(isDev
+                ? [
+                    {
+                        protocol: 'http',
+                        hostname: '127.0.0.1',
+                    },
+                    {
+                        protocol: 'http',
+                        hostname: 'localhost',
+                    },
+                ]
+                : []),
             {
-              protocol: 'http',
-              hostname: '127.0.0.1',
+                protocol: 'http',
+                hostname: '91.135.156.15',
+                port: '',
+                pathname: '/media/**',
             },
             {
-              protocol: 'http',
-              hostname: 'localhost',
+                protocol: 'http',
+                hostname: '91.135.156.15',
+                port: '',
+                pathname: '/mock/**',
             },
-          ]
-        : []),
-      {
-        protocol: 'http',
-        hostname: '91.135.156.15',
-        port: '',
-        pathname: '/media/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '91.135.156.15',
-        port: '',
-        pathname: '/mock/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '91.135.156.15',
-        port: '',
-        pathname: '/icons/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '91.135.156.15',
-        port: '',
-        pathname: '/logo/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '91.135.156.15',
-        port: '',
-        pathname: '/api/v0/**',
-      },
-    ],
-  },
+            {
+                protocol: 'http',
+                hostname: '91.135.156.15',
+                port: '',
+                pathname: '/icons/**',
+            },
+            {
+                protocol: 'http',
+                hostname: '91.135.156.15',
+                port: '',
+                pathname: '/logo/**',
+            },
+            {
+                protocol: 'http',
+                hostname: '91.135.156.15',
+                port: '',
+                pathname: '/api/v0/**',
+            },
+        ],
+    },
 };
